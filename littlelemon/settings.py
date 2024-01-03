b8812c12a7227a88e181e57694151cccdb53bda9
vamp1r3kn1ght <brian.nguyen271297@gmail.com>
2024-01-03 14:11:10 -0500

    models

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "little_lemon",
+        "HOST" : "127.0.0.1",
+        "PORT" : "3306",
+        "USER" : "root",
+        "PASSWORD": "Minhanh1804@",
+        "OPTIONS" : {
+            "init_command" : "SET sql_mode = 'STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -117,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'restaurant/static/';
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
   "restaurant/static"  
 ];
 
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length = 255);
+    no_of_guests = models.PositiveSmallIntegerField();
+    booking_date = models.DateTimeField();
+    
+    def __str__(self) -> str:
+        return self.name;
+    
+class Menu(models.Model):
+    title = models.CharField(max_length = 255);
+    price = models.DecimalField(max_digits = 10, decimal_places = 2);
+    inventory = models.PositiveSmallIntegerField();
+    
+    def __str__(self) -> str:
+        return f"{self.title}, In Stock: {self.inventory}";
